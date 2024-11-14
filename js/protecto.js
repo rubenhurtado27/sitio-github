2f81a3fd9372666978aaecb7234194dfc486e1d5
rubenhurtado27 <alejosola68pte@gmail.com>
2024-11-14 11:02:07 -0500

    Add files via upload

---
+++ js/protecto.js
@@ -0,0 +1,13 @@
+const hamburguer = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu-navegacion');
+
+hamburguer.addEventListener('click', () => {
+    menu.classList.toggle("spread");
+});
+
+window.addEventListener('click', e => {
+    if (menu.classList.contains('spread') 
+        && e.target != menu && e.target != hamburguer) {
+        menu.classList.toggle("spread");
+    }
+});
